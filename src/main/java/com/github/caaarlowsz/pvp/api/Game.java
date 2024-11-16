0a3491d4091ce1630a325e92ab309d54bcd4fd67
Caaarlowsz <caaarlowsz@outlook.com>
2024-11-16 17:47:44 -0300

    Adicionando métodos de players a interface principal.

--- src/main/java/com/github/caaarlowsz/pvp/api/Game.java
+++ src/main/java/com/github/caaarlowsz/pvp/api/Game.java
@@ -4,6 +4,10 @@ import java.util.Set;
 
 public interface Game {
 
+	public Set<Player> getPlayers();
+
+	public Player getPlayer(org.bukkit.entity.Player player);
+
 	public Set<Kit> getKits();
 
 	public Kit getKit(String name);
